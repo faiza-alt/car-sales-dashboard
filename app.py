97c71758f5e5b6f2ec55475778341c49644bf662
faiza-alt <69270809+faiza-alt@users.noreply.github.com>
2024-07-29 11:36:02 -0400

    initial commit

--- app.py
+++ app.py
@@ -1,27 +1,53 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 
-# Load the dataset
 df = pd.read_csv('vehicles_us.csv')
+df['manufacturer'] = df['model'].apply(lambda x: x.split()[0])
 
-# App title
-st.title('Car Sales Dashboard')
+st.header('Data viewer')
+show_manuf_1k_ads = st.checkbox('Include manufacturers with less than 1000 ads')
+if not show_manuf_1k_ads:
+    df = df.groupby('manufacturer').filter(lambda x: len(x) > 1000)
 
-# Header
-st.header('Price Distribution')
+st.dataframe(df)
+st.header('Vehicle types by manufacturer')
+st.write(px.histogram(df, x='manufacturer', color='type'))
+st.header('Histogram of `condition` vs `model_year`')
 
-# Plot histogram
-fig = px.histogram(df, x='price')
-st.plotly_chart(fig)
+# -------------------------------------------------------
+# histograms in plotly:
+# fig = go.Figure()
+# fig.add_trace(go.Histogram(x=df[df['condition']=='good']['model_year'], name='good'))
+# fig.add_trace(go.Histogram(x=df[df['condition']=='excellent']['model_year'], name='excellent'))
+# fig.update_layout(barmode='stack')
+# st.write(fig)
+# works, but too many lines of code
+# -------------------------------------------------------
 
-# Header
-st.header('Price vs Year')
+# histograms in plotly_express:
+st.write(px.histogram(df, x='model_year', color='condition'))
+# a lot more concise!
+# -------------------------------------------------------
 
-# Plot scatter plot
-fig = px.scatter(df, x='year', y='price', color='model')
-st.plotly_chart(fig)
+st.header('Compare price distribution between manufacturers')
+manufac_list = sorted(df['manufacturer'].unique())
+manufacturer_1 = st.selectbox('Select manufacturer 1',
+                              manufac_list, index=manufac_list.index('chevrolet'))
 
-# Checkbox to show/hide raw data
-if st.checkbox('Show raw data'):
-    st.write(df)
+manufacturer_2 = st.selectbox('Select manufacturer 2',
+                              manufac_list, index=manufac_list.index('hyundai'))
+mask_filter = (df['manufacturer'] == manufacturer_1) | (df['manufacturer'] == manufacturer_2)
+df_filtered = df[mask_filter]
+normalize = st.checkbox('Normalize histogram', value=True)
+if normalize:
+    histnorm = 'percent'
+else:
+    histnorm = None
+st.write(px.histogram(df_filtered,
+                      x='price',
+                      nbins=30,
+                      color='manufacturer',
+                      histnorm=histnorm,
+                      barmode='overlay'))
